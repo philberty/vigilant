b4f72756da9b44bb6cb6d839c7c8d6ba8e4e40ff
Philip <redbrain@gcc.gnu.org>
2014-05-23 10:37:00 +0100

    Basic cython module for libwatchy.so

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
+import sys
+import pkgconfig
 from distutils.core import setup
+from Cython.Build import cythonize
+
+if pkgconfig.exists ('watchy') is False:
+    print >> sys.stderr, "Make sure pkg-config watchy --cflags --libs works."
+    sys.exit (1)
+watchy = pkgconfig.parse ('watchy')
 
 setup (
     name = "Watchy",
@@ -9,6 +16,10 @@ setup (
     author_email = 'redbrain@gcc.gnu.org',
     license = "MIT",
     description = 'A stats agregation daemon over UDP',
+    ext_modules = cythonize ('pywatchy', ['bindings/python/watchy.pyx'],
+                             include_dirs = watchy ['include_dirs'],
+                             libraries = watchy ['libraries'],
+                             library_dirs = watchy ['library_dirs'])
     packages = ['WatchyServer'],
     scripts = ['watchy.py'],
     package_dir = {'WatchyServer': 'WatchyServer'},

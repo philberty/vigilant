87e8e7a894e66e681b72cff879763ca253482c3f
Philip <redbrain@gcc.gnu.org>
2014-06-02 14:24:17 +0100

    dejagnu in automake options and fix logpacket

--- src/libwatchy.c
+++ src/libwatchy.c
@@ -76,13 +76,8 @@ watchy_logPacket (struct watchy_data * const data, const char * message, const c
   char buffer [sizeof (data->value.buffer)];
   memset (buffer, 0, sizeof (buffer));
 
-  size_t ncpy = 0;
-  if (strlen (message) >= sizeof (buffer))
-    ncpy = sizeof (buffer) - 1;
-  else
-    ncpy = strlen (message) - 1;
+  size_t ncpy = (strlen (message) >= sizeof (buffer)) ? sizeof (buffer) - 1 : strlen (message);
   strncpy (buffer, message, ncpy);
-
   strncpy (data->value.buffer, buffer, sizeof (buffer));
 }
 

07dbcf38793c273be2a53bf7e758328af2d2fd47
Philip Herron <philip.herron@instil.co>
2014-11-20 17:18:48 +0000

    Redo app into:
      1. Python Stats Daemon + Bindings
      2. Java Server for stats aggregator and rest service - War
      3. Nodejs Front-end Application using angularjs

--- src/main/java/io/github/observant/servlets/HostDataServlet.java
+++ src/main/java/io/github/observant/servlets/HostDataServlet.java
@@ -3,5 +3,6 @@ package io.github.observant.servlets;
 /**
  * Created by redbrain on 20/11/2014.
  */
-public class HostResourceServlet {
+public class HostDataServlet {
+
 }
--- src/main/webapp/js/observant.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by redbrain on 19/11/2014.
- */
-console.log('Helloworld')

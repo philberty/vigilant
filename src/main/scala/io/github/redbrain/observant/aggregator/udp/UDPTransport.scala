84b815e12a95907210957502bc4f74d6c9fe6bd7
Philip Herron <philip.herron@instil.co>
2014-11-25 14:55:35 +0000

    Expressjs skelton dashbord

--- src/main/scala/io/github/redbrain/observant/aggregator/udp/UDPTransport.scala
+++ src/main/scala/io/github/redbrain/observant/aggregator/udp/UDPTransport.scala
@@ -14,28 +14,28 @@ import org.slf4j.LoggerFactory
  */
 class UDPTransport(private val port: Int) extends TransportType {
 
-  private var _factory: NioDatagramChannelFactory = null
-  private var _bootstrap: ConnectionlessBootstrap = null
+  private var factory: NioDatagramChannelFactory = null
+  private var bootstrap: ConnectionlessBootstrap = null
   private val logger = LoggerFactory.getLogger(getClass)
 
   def close(): Unit = {
-    _factory.shutdown()
-    _bootstrap.shutdown()
+    factory.shutdown()
+    bootstrap.shutdown()
     logger.info("UDP Stats transport shutdown on port [{}]", port)
   }
 
   def start(): Unit = {
-    _factory = new NioDatagramChannelFactory(Executors.newCachedThreadPool())
-    _bootstrap = new ConnectionlessBootstrap(_factory)
-    _bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+    factory = new NioDatagramChannelFactory(Executors.newCachedThreadPool())
+    bootstrap = new ConnectionlessBootstrap(factory)
+    bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
       def getPipeline(): ChannelPipeline = {
         Channels.pipeline(StatsAggregator)
       }
     })
 
-    _bootstrap.setOption("child.tcpNoDelay", true)
-    _bootstrap.setOption("child.keepAlive", true)
-    _bootstrap.bind(new InetSocketAddress(port))
+    bootstrap.setOption("child.tcpNoDelay", true)
+    bootstrap.setOption("child.keepAlive", true)
+    bootstrap.bind(new InetSocketAddress(port))
 
     logger.info("UDP Stats transport ready on port [{}]", port)
   }

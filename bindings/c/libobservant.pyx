c2fabc378afba4f66f361e8e93b16f0704bfe961
Philip Herron <philip.herron@instil.co>
2014-12-11 12:25:04 +0000

    Initial C/C++ bindings

---
+++ bindings/c/libobservant.pyx
@@ -0,0 +1,41 @@
+import StatsCore
+
+from posix.unistd cimport pid_t
+
+Daemon = None
+
+cdef public int obs_attach_to_stats_daemon(const char *pid, const char *sock):
+    global Daemon
+    try:
+        Daemon = StatsCore.attchToStatsDaemon(pid=pid, sock=sock)
+        return 0
+    except:
+        return -1
+
+cdef public int obs_watch_pid(const pid_t pid, const char *key):
+    global Daemon
+    if Daemon is None:
+        return -1
+    Daemon.postWatchPid(key, pid)
+    return 0
+
+cdef public int obs_stop_watch_pid(const pid_t pid):
+    global Daemon
+    if Daemon is None:
+        return -1
+    Daemon.postStopWatchPid(pid)
+    return 0
+
+cdef public int obs_stop_watch_key(const char *key):
+    global Daemon
+    if Daemon is None:
+        return -1
+    Daemon.postStopWatchKey(key)
+    return 0
+
+cdef public int obs_post_log_message(const char *key, const char *message):
+    global Daemon
+    if Daemon is None:
+        return -1
+    Daemon.postLogMessageForKey(key. message)
+    return 0
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-from StatsCore import StatsDaemonState
 from distutils.core import setup
 
 setup(name='StatsCore',
-      version=StatsDaemonState.STATS_DAEMON_VERSION,
+      version='0.1',
       description='Observant Stats Daemon and Client',
       author='Philip Herron',
       author_email='herron.philip@googlemail.com',

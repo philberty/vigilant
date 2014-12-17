c9668b9adeee6b4d783581bfb44a757566633fa5
Philip Herron <philip.herron@instil.co>
2014-12-17 17:03:02 +0000

    Initial index concept

--- StatsBoard/www/js/observant.js
+++ StatsBoard/www/js/observant.js
@@ -1,37 +1,42 @@
 require.config({
     paths: {
         jquery: [
-            '//code.jquery.com/jquery-2.1.1.min',
+            //'//code.jquery.com/jquery-2.1.1.min',
             '/js/lib/jquery/dist/jquery.min'],
 
         vis: [
-            '//cdnjs.cloudflare.com/ajax/libs/vis/3.7.1/vis.min',
+            //'//cdnjs.cloudflare.com/ajax/libs/vis/3.7.1/vis.min',
             '/js/lib/vis/dist/vis.min'
         ],
 
         bootstrap: [
-            '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min',
+            //'//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min',
             '/js/lib/bootstrap/dist/js/bootstrap'],
 
         angular: [
-            '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.min',
+            //'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.min',
             '/js/lib/angular/angular'],
 
         angularRoute: [
-            '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-route.min',
+            //'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-route.min',
             '/js/lib/angular-route/angular-route'],
 
         angularBootstrap: [
-            '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min',
-            '/js/lib/angular-bootstrap/ui-bootstrap-tpls']
+            //'//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min',
+            '/js/lib/angular-bootstrap/ui-bootstrap-tpls'],
+
+        angularLoadingBar: [
+            '/js/lib/angular-loading-bar/build/loading-bar.min'
+        ]
     },
     shim: {
-        'vis': {
-          exports: 'vis'
-        },
         'bootstrap': {
             deps: ['jquery'],
             exports: 'bootstrap'
+        },
+        'angularLoadingBar': {
+            deps: ['angular'],
+            exports: 'angularLoadingBar'
         },
         'angularBootstrap': {
             deps: ['angular'],
@@ -49,8 +54,11 @@ require.config({
     deps: ['app']
 });
 
-define('app', ["jquery", "angular", "vis", "angularBootstrap", "angularRoute", "bootstrap"], function($, angular) {
-    var app = angular.module("ObservantApp", ['ngRoute', 'ui.bootstrap']);
+define('app', ["jquery", "angular", "vis", "angularBootstrap",
+                "angularRoute", "angularLoadingBar", "bootstrap"],
+    function($, angular, vis)
+{
+    var app = angular.module("ObservantApp", ['ngRoute', 'ui.bootstrap', 'angular-loading-bar']);
 
     app.config(
         ['$routeProvider',
@@ -73,16 +81,44 @@ define('app', ["jquery", "angular", "vis", "angularBootstrap", "angularRoute", "
     app.controller('dashboard', function($scope, $http, $interval) {
         var state = function () {
             $http.get('/api/state').success(function(data) {
-                $scope.datastores = []
-                for(var key in data){
-                    $scope.datastores.push(key);
-                    $scope[key] = data.key;
-                }
+                $scope.data = data;
+                $scope.datastores = [];
+                for(var key in data) $scope.datastores.push(
+                    {
+                        key: key,
+                        name: decodeURIComponent(key)
+                    }
+                );
             });
         };
-
         state();
         $interval(state, 5000);
+
+        // create an array with nodes
+        var nodes = [
+            {id: 1, label: 'Node 1'},
+            {id: 2, label: 'Node 2'},
+            {id: 3, label: 'Node 3'},
+            {id: 4, label: 'Node 4'},
+            {id: 5, label: 'Node 5'}
+        ];
+
+        // create an array with edges
+        var edges = [
+            {from: 1, to: 2},
+            {from: 1, to: 3},
+            {from: 2, to: 4},
+            {from: 2, to: 5}
+        ];
+
+        // create a network
+        var container = document.getElementById('mynetwork');
+        var data = {
+            nodes: nodes,
+            edges: edges
+        };
+        var options = {};
+        var network = new vis.Network(container, data, options);
     });
 
     angular.bootstrap(document, ['ObservantApp']);

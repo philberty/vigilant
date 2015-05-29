cf93d405fbe868989d6e894124d5a46cb6cd4024
Philip Herron <herron.philip@googlemail.com>
2015-05-29 14:47:22 +0100

    Small update

--- daemon/jenkins.sh
+++ daemon/jenkins.sh
@@ -3,5 +3,6 @@ set -e
 
 virtualenv env
 . env/bin/activate
+pip install -r requirements.txt
 python setup.py build
 python setup.py install
--- front-end/jenkins.sh
+++ front-end/jenkins.sh
@@ -2,8 +2,10 @@
 set -e
 
 bower install
+
 virtualenv env
 . env/bin/activate
+pip install -r requirements.txt
 python setup.py build
 
 # add karma
